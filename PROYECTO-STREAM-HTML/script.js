e9c92e65052711ab71cb8e290cb6b3704349c496
Lesly1234 <127535084+Lesly1234@users.noreply.github.com>
2023-03-10 12:04:26 -0500

    Add files via upload

---
+++ PROYECTO-STREAM-HTML/script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje, 2000)
+
+function cambiaMensaje() {
+    document.getElementById("loading-message").innerText = "Enviado!"
+}
+
+setTimeout(function () {
+   alert("Recibiste un abrazo y muchos besitos!!! , Solo para que no olvides que Te quiero infinitas vueltas al mundo 🌎❤️")
+}, 2400)
